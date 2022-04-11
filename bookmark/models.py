385c9594bc9786a75f748ef66ce5ac8a097bd052
BaekJeongEun <sophia991106@gmail.com>
2022-04-12 01:44:20 +0900

    create Bookmark model

---
+++ bookmark/models.py
@@ -0,0 +1,6 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
